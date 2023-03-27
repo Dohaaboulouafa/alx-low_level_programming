3b6a32ab84171bd1da60ca2a24485a9ae55acde5
Dohaaboulouafa <doha.aboulouafaa@gmail.com>
2023-03-27 15:35:23 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reser_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * Owned by Bwave/Bright
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n - 98;
+}
